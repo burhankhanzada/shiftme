89131d9e4c1f96d4fddd311b033d793d12153810
Burhan Khanzada <burhankhanzada199888@gmail.com>
2021-11-11 00:04:50 +0500

    created basic home screen from matching prototype design

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:shiftme/src/screens/home_screen.dart';
+import 'package:spaces/spaces.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      builder: (context, child) => Spacing.fixed(
+        data: SpacingData.generate(10.0),
+        child: child!,
+      ),
+      theme: ThemeData(
+          primaryColor: Colors.brown,
+          accentColor: Colors.orange,
+          colorScheme: const ColorScheme.light().copyWith(
+            primary: Colors.brown,
+            secondary: Colors.orange,
+          )),
+      home: const HomeScreen(),
     );
   }
 }
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,10 +1,122 @@
 import 'package:flutter/material.dart';
+import 'package:spaces/spaces.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("ShiftMe"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.filter_list),
+            onPressed: () {},
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SpacedRow.semiBig(
+                  children: [
+                    const CircleAvatar(
+                      backgroundColor: Colors.brown,
+                      radius: 36,
+                    ),
+                    Text(
+                      "User Name",
+                      style: Theme.of(context).textTheme.headline6,
+                    )
+                  ],
+                ),
+              ),
+              const ListTile(
+                title: Text("Profile"),
+              ),
+              const ListTile(
+                title: Text("Bookings"),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: SpacedColumn(
+        padding: const EdgeInsets.all(16),
+        children: [
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'From',
+            ),
+          ),
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'To',
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: const [
+                Card(
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: SpacedColumn(
+                      padding: EdgeInsets.all(8),
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Driver Name: Ali"),
+                        Text("Vechile: Shezor"),
+                        Text("Load Capacity: 500kg"),
+                        Text("Availability: 10AM - 8PM"),
+                        Text("Starting Price: 500"),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: SpacedColumn(
+                      padding: EdgeInsets.all(8),
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Driver Name: Ali"),
+                        Text("Vechile: Ravi"),
+                        Text("Load Capacity: 500kg"),
+                        Text("Availability: 10AM - 8PM"),
+                        Text("Starting Price: 500"),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: SpacedColumn(
+                      padding: EdgeInsets.all(8),
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Driver Name: Ali"),
+                        Text("Vechile: Mazda"),
+                        Text("Load Capacity: 500kg"),
+                        Text("Availability: 10AM - 8PM"),
+                        Text("Starting Price: 500"),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
